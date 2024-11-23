2584c83c18eaaaa25f7143273fb795ebb391a238
Khadeeja <kgamieldien23@gmail.com>
2024-11-23 21:23:53 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const loadingOverlay = document.getElementById('loading-overlay');
     
-    // Hide the loading overlay after a short delay to ensure everything is fully loaded
+
     setTimeout(function() {
         loadingOverlay.style.opacity = '0';
         setTimeout(function() {
             loadingOverlay.style.display = 'none';
-        }, 300); // Match the transition duration in CSS
-    }, 500); // Adjust the delay as needed
+        }, 300); 
+    }, 500); 
 });
